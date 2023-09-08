b5f604bca3ab3f110929a8cf16b75148b4c48046
SyedIrfanali03 <greenstaskmay@gmail.com>
2023-09-08 15:39:06 +0530

    preeti added few lines

--- src/com/selenium/Selenium.java
+++ src/com/selenium/Selenium.java
@@ -3,6 +3,14 @@ package com.selenium;
 public class Selenium {
 
 	
+	private void preeti() {
+		System.out.println("Hi am preethi ");
+		
+		System.out.println("How are you ");
+
+	}
+	
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Task 1");
